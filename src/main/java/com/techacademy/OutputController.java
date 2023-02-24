bc541d1123c0c7bcffef618479a4aea2ec44cfb0
syaketaro <a.tomm722@icloud.com>
2023-02-25 00:06:16 +0900

    コミット

---
+++ src/main/java/com/techacademy/OutputController.java
@@ -0,0 +1,26 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OutputController {
+	
+	/*@PostMapping("/output")
+	public String postOutput(Model model) {
+		model.addAttribute("id", "previous");
+		return "output";
+	}
+	*/
+	
+	@PostMapping("/output")
+	public String postOutput(@RequestParam("previous") String previous, Model model) {
+		
+		model.addAttribute("previous", previous);
+		
+		return "output";
+	}
+}
